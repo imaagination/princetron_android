32f357c8bc3627f10b7e1f38c9b8f4d885b3559a
Andy Kaier <akaier@princeton.edu>
2012-03-01 13:18:12 -0500

    Checked in Game Network Abstract Class

---
+++ src/princeTron/Network/GameNetwork.java
@@ -0,0 +1,8 @@
+import java.awt.Point;
+
+public abstract class GameNetwork
+{
+    abstract void startGame(int wait_time); 
+    abstract void opponentTurn(int snake_id, Point position, int time, boolean isLeft);
+    abstract void gameOver(boolean wonGame);
+}
