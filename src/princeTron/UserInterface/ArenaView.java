da594abb7fe996883712b050847f084c12e1459e
Kashif Smith <kashifssmith@dynamic-oit-pu-dormnet-bb-f-35.Princeton.EDU>
2012-03-15 11:57:14 -0400

    added touch sensor to Arena UI

--- src/princeTron/UserInterface/ArenaView.java
+++ src/princeTron/UserInterface/ArenaView.java
@@ -5,10 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class ArenaView extends View {
+public class ArenaView extends View{
 	private int xMin = 0;          // This view's bounds
 	private int xMax;
 	private int yMin = 0;
@@ -23,30 +26,84 @@ public class ArenaView extends View {
 	private int duration = Toast.LENGTH_LONG;
 	private Toast toast;
 	private CharSequence test1;
+	private int height, width;
 
 	// Constructor
-	public ArenaView(Context context) {
-		super(context);
-		this.context = context;
+	public ArenaView(Context context1, int height1, int width1) {
+		super(context1);
+		this.context = context1;
 		paint = new Paint();
 		paint.setColor(Color.rgb(255,140,0));
 		paint.setStrokeWidth(6);
-//		paint.setStrokeCap(Paint.Cap.ROUND);
 		pointsVisited = new float[1][5000];
 		pointIndex = 0;
+		this.height = height1;
+		this.width = width1;
+
+
+
+		ImageButton leftButton = new ImageButton(context);
+		leftButton.getBackground().setVisible(false, false);
+
+		this.setOnTouchListener(new OnTouchListener(){
+			public boolean onTouch(View v, MotionEvent event){
+				Toast toast;
+				float x;
+				float y;       
+				CharSequence text;
+
+				switch (event.getAction())
+				{
+				case MotionEvent.ACTION_DOWN:
+				{
+					x = event.getX();
+					y = event.getY();     
+
+					if(x > width/2.0)
+						text = "Right button pressed!"; //call rightPressed Method
+					else
+						text = "Left button pressed!"; //call leftPressed Method
+
+
+
+					toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);  
+					toast.show();
+
+					break;
+				}
+				case MotionEvent.ACTION_MOVE:
+				{
+					x = event.getX();
+					y = event.getY();
+
+
+					CharSequence xText = "This is the x coord: " + x;
+					CharSequence yText = "This is the y coord: " + y;
+
+					toast = Toast.makeText(context, xText, Toast.LENGTH_SHORT);  
+					toast.show();
+					toast = Toast.makeText(context, yText, Toast.LENGTH_SHORT);  
+					toast.show();
+
+					break;
+				}
+				}
+				return true;
+			}
+		});
 
 	}
 
 	public void addPoints(float[] points){
 		int j = 0;
 
-//		test1 = "Number of players: " + numPlayers;
-//		toast = Toast.makeText(context, test1, duration);
-//		toast.show();
+		//		test1 = "Number of players: " + numPlayers;
+		//		toast = Toast.makeText(context, test1, duration);
+		//		toast.show();
 
 		//update player positions
-			pointsVisited[0][pointIndex++] = points[j++];
-			pointsVisited[0][pointIndex++] = points[j];
+		pointsVisited[0][pointIndex++] = points[j++];
+		pointsVisited[0][pointIndex++] = points[j];
 	}
 
 	// Called back to draw the view. Also called by invalidate().
@@ -54,19 +111,20 @@ public class ArenaView extends View {
 	protected void onDraw(Canvas canvas) {
 
 		xCoord++;
-		
+
 		// Update the position of the head, including collision detection and reaction.
 		update(xCoord, yCoord);
 
-		
+
 		canvas.drawPoints(pointsVisited[0], paint);
-	
+
+
 
 
 		// Delay
-//		try {  
-//			Thread.sleep(10);  
-//		} catch (InterruptedException e) { }
+		//		try {  
+		//			Thread.sleep(10);  
+		//		} catch (InterruptedException e) { }
 
 
 		invalidate();  // Force a re-draw
@@ -92,13 +150,16 @@ public class ArenaView extends View {
 			yCoord = 0;
 		}
 
-//		float[] points = new float[(numPlayers*2)];
-		
+		//		float[] points = new float[(numPlayers*2)];
+
 		float[] points = {xCoord,yCoord};
-		
+
 
 		addPoints(points);
 	}
+
+
+
 
 
 	// Called back when the view is first created or its size changes.
--- src/princeTron/UserInterface/PrinceTronActivity.java
+++ src/princeTron/UserInterface/PrinceTronActivity.java
@@ -1,16 +1,60 @@
 package princeTron.UserInterface;
 import android.app.Activity;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class PrinceTronActivity extends Activity {
-	   /** Called when the activity is first created. */
-	   @Override
-	   public void onCreate(Bundle savedInstanceState) {
-	      super.onCreate(savedInstanceState);
-	      View arena = new ArenaView(this);
-	      setContentView(arena);
-	   }
+public class PrinceTronActivity extends Activity{
+
+	RelativeLayout _main;
+	int tapCount = 0;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		_main = (RelativeLayout)findViewById(R.id.relativeLayout1);
+
+
+		//		Button leftButton = (Button)findViewById(R.id.leftButton);
+		//		Button rightButton = (Button)findViewById(R.id.rightButton);
+		//		leftButton.setOnClickListener(new OnClickListener() {
+		//			public void onClick(View v) {
+		//				Toast toast=Toast.makeText(getApplicationContext(), "Left button pressed", Toast.LENGTH_SHORT);  
+		//				toast.show();
+		//			}
+		//		});
+		//		rightButton.setOnClickListener(new OnClickListener() {
+		//			public void onClick(View v) {
+		//				Toast toast=Toast.makeText(getApplicationContext(), "Right button pressed", Toast.LENGTH_SHORT);  
+		//				toast.show();
+		//			}
+		//		});
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+		int height = metrics.heightPixels;
+		int width = metrics.widthPixels;
+		
+		
+		View arena = new ArenaView(this, height, width);
+		setContentView(arena);
+		
+		
+
+	}
+
 }
 
 
@@ -19,4 +63,5 @@ public class PrinceTronActivity extends Activity {
 
 
 
-    
+
+
