ed69171ccfa52bf2caf9d64a1c10e602829b6784
kashifssmith <kashifssmith@dynamic-oit-pu-dormnet-bb-f-190.Princeton.EDU>
2012-05-11 18:02:11 -0400

    -profile update

--- src/princeTron/UserInterface/Profile.java
+++ src/princeTron/UserInterface/Profile.java
@@ -21,6 +21,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Profile extends Activity{
 	/** Called when the activity is first created. */
@@ -116,8 +117,9 @@ public class Profile extends Activity{
 			buf.close();
 			ips.close();
 			
-			//is there a better way to do this?
-			String input = sb.toString().replace("&quot;", "\"");
+			
+			String input = sb.toString();
+//			Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
 			JSONObject j = new JSONObject(input);
 
 			values[0] = j.getInt("joined_day");
@@ -162,8 +164,7 @@ public class Profile extends Activity{
 			ips.close();
 
 			
-			//is there a better way to do this?
-			String input = sb.toString().replace("&quot;", "\"");
+			String input = sb.toString();
 			JSONObject j = new JSONObject(input);
 
 			values[0] = j.getInt("joined_day");
